c0dd6ffb14b66c202e6a80fb5012ad1da633501b
MOEKASAKAI <wpi.floves2@gmail.com>
2021-02-11 13:52:59 +0900

    [Start] JS-6

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// アニメーションを入れる場合はslideDown()の第一因数に数字を入力する
 $(function(){
   $('.box1').slideDown(1000,function (){
     $('.box1').css({
@@ -8,6 +9,9 @@ $(function(){
   });
 });
 
-// アニメーションを入れる場合はslideDown()の第一因数に数字を入力する
-
-
+// jQueryで「マウスオーバー」イベントを設定する
+$(function(){
+  $('.box2').mouseover(function(){
+    $('.box2').css({'background-color': '#0000FF'});
+  });
+});
