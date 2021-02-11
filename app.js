7b9fe1408f81d1ebce15661c911a5d6f7d157142
MOEKASAKAI <wpi.floves2@gmail.com>
2021-02-11 14:09:45 +0900

    [Finish] JS-6

--- app.js
+++ app.js
@@ -14,4 +14,28 @@ $(function(){
   $('.box2').mouseover(function(){
     $('.box2').css({'background-color': '#0000FF'});
   });
+  $('.box2').mouseout(function(){
+    $('.box2').css({'background-color' : '#FF0000'});
+  });
+});
+
+// class属性を追加する／解除する
+$(function(){
+  $('.box2').mouseover(function(){
+    $('.box2').addClass('box2-ext');
+  });
+  $('.box2').mouseout(function(){
+    $('.box2').removeClass('box2-ext');
+  });
+});
+
+// 「マウスクリック」イベントを設定する
+//  クリック後に複数のイベントが行われるようにしたいので、onを付けて記入
+$(function(){
+  $('.box2').on('click', function(){
+    $('.bos2').removeClass('box2-ext');
+  });
+  $('.box2').mouseout(function(){
+    $('.box2').addClass('box2-ext');
+  });
 });
